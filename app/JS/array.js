a8d936f533d2c11741e9273982e83c34e77965d1
Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
2024-12-11 12:33:02 -0500

    a

---
+++ app/JS/array.js
@@ -0,0 +1,48 @@
+export const exercises = [
+  // Chest Exercises (Difficulty 1-4)
+  { name: "Incline Push-ups", difficulty: 1, bodyPart: "Chest", id: "45D" },
+  { name: "Chest Fly", difficulty: 2, bodyPart: "Chest", id: "34C" },
+  { name: "Push-ups", difficulty: 3, bodyPart: "Chest", id: "12A" },
+  { name: "Decline Push-ups", difficulty: 4, bodyPart: "Chest", id: "56E" },
+  { name: "Bench Press", difficulty: 4, bodyPart: "Chest", id: "23B" },
+
+  // Back Exercises (Difficulty 2-5)
+  { name: "Superman Exercise", difficulty: 2, bodyPart: "Back", id: "90I" },
+  { name: "Bent-over Rows", difficulty: 3, bodyPart: "Back", id: "78G" },
+  { name: "Reverse Fly", difficulty: 3, bodyPart: "Back", id: "01J" },
+  { name: "Lat Pulldowns", difficulty: 4, bodyPart: "Back", id: "89H" },
+  { name: "Pull-ups", difficulty: 5, bodyPart: "Back", id: "67F" },
+
+  // Arm Exercises (Difficulty 2-4)
+  { name: "Bicep Curls", difficulty: 2, bodyPart: "Arms", id: "12K" },
+  { name: "Hammer Curls", difficulty: 2, bodyPart: "Arms", id: "34M" },
+  { name: "Tricep Dips", difficulty: 3, bodyPart: "Arms", id: "23L" },
+  {
+    name: "Overhead Tricep Extensions",
+    difficulty: 3,
+    bodyPart: "Arms",
+    id: "45N",
+  },
+  { name: "Concentration Curls", difficulty: 4, bodyPart: "Arms", id: "56O" },
+
+  // Leg Exercises (Difficulty 2-5)
+  { name: "Squats", difficulty: 2, bodyPart: "Legs", id: "67P" },
+  { name: "Lunges", difficulty: 3, bodyPart: "Legs", id: "78Q" },
+  { name: "Step-Ups", difficulty: 3, bodyPart: "Legs", id: "01T" },
+  { name: "Leg Press", difficulty: 4, bodyPart: "Legs", id: "90S" },
+  { name: "Deadlifts", difficulty: 5, bodyPart: "Legs", id: "89R" },
+
+  // Cardio Exercises (Difficulty 1-5)
+  { name: "Jumping Jacks", difficulty: 1, bodyPart: "Cardio", id: "23V" },
+  { name: "High Knees", difficulty: 2, bodyPart: "Cardio", id: "34W" },
+  { name: "Mountain Climbers", difficulty: 3, bodyPart: "Cardio", id: "45X" },
+  { name: "Burpees", difficulty: 4, bodyPart: "Cardio", id: "12U" },
+  { name: "Sprints", difficulty: 5, bodyPart: "Cardio", id: "56Y" },
+
+  // Shoulder Exercises (Difficulty 1-5)
+  { name: "Shrugs", difficulty: 1, bodyPart: "Shoulders", id: "01D" },
+  { name: "Front Raises", difficulty: 2, bodyPart: "Shoulders", id: "89B" },
+  { name: "Lateral Raises", difficulty: 3, bodyPart: "Shoulders", id: "78A" },
+  { name: "Overhead Press", difficulty: 4, bodyPart: "Shoulders", id: "67Z" },
+  { name: "Arnold Press", difficulty: 5, bodyPart: "Shoulders", id: "90C" },
+];
